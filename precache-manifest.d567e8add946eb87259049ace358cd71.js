9995824a4f7e9b827f18380cbd549ec18ab9c750
JHankins09 <Hankins.JamesB@gmail.com>
2019-12-09 17:17:49 -0800

    Updates

---
+++ precache-manifest.d567e8add946eb87259049ace358cd71.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "836c3e9a05a270f41f516b771bead9d2",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "dbad766d5a43d8128022",
+    "url": "/portfolio/static/css/main.c424836c.chunk.css"
+  },
+  {
+    "revision": "71e21b33e3858d0dfd6a",
+    "url": "/portfolio/static/js/2.95c20fa4.chunk.js"
+  },
+  {
+    "revision": "dbad766d5a43d8128022",
+    "url": "/portfolio/static/js/main.25696680.chunk.js"
+  },
+  {
+    "revision": "8f13fc4a443b56dd2096",
+    "url": "/portfolio/static/js/runtime-main.3a5a3038.js"
+  },
+  {
+    "revision": "80a440de057859b34b0133fa1989c9ff",
+    "url": "/portfolio/static/media/AboutMe.80a440de.jpg"
+  },
+  {
+    "revision": "f6faa4cc49534f1cd2b6fefd66b68226",
+    "url": "/portfolio/static/media/BG-Portfolio.f6faa4cc.jpg"
+  },
+  {
+    "revision": "40dc737b188ed865edc899e88f0810b6",
+    "url": "/portfolio/static/media/ChickenPix.40dc737b.png"
+  },
+  {
+    "revision": "7a3edcf113c9d20f97fbb041c0b2bfc6",
+    "url": "/portfolio/static/media/DadJokeApp.7a3edcf1.png"
+  },
+  {
+    "revision": "d24f4039093c3479827c83a7309c3616",
+    "url": "/portfolio/static/media/Groups.d24f4039.png"
+  },
+  {
+    "revision": "be150cc2e3d818fd948dd712eeac5cf7",
+    "url": "/portfolio/static/media/Portfolio.be150cc2.png"
+  },
+  {
+    "revision": "e22e1ce85dae80c839982d642d6b13cd",
+    "url": "/portfolio/static/media/Resume_J-Hankins.e22e1ce8.pdf"
+  },
+  {
+    "revision": "08a714c62c451301904b33c4bf8798db",
+    "url": "/portfolio/static/media/Tic-Tac-Toe.08a714c6.png"
+  },
+  {
+    "revision": "62622aa857445b26c5ee5a0bd437c8af",
+    "url": "/portfolio/static/media/Trips.62622aa8.png"
+  },
+  {
+    "revision": "6f7c0b121faa2534ee7f7a37a7c0a0ce",
+    "url": "/portfolio/static/media/WebChat.6f7c0b12.png"
+  },
+  {
+    "revision": "33e5da34561db816a197c73a2d998ddd",
+    "url": "/portfolio/static/media/photo.33e5da34.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.468ffabcd3f55cec49441ec71c4ab343.js"
+  "/portfolio/precache-manifest.d567e8add946eb87259049ace358cd71.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
